47a49a2680cc4405d95d379fec53fa4867cd1025
Pedro Conceição <1211018@isep.ipp.pt>
2024-11-02 20:05:16 +0000

    Fixed BookTest, now they are running

--- src/test/java/pt/psoft/g1/psoftg1/unitTests/bookmanagement/model/BookTest.java
+++ src/test/java/pt/psoft/g1/psoftg1/unitTests/bookmanagement/model/BookTest.java
@@ -1,7 +1,10 @@
 package pt.psoft.g1.psoftg1.unitTests.bookmanagement.model;
 
+import com.google.rpc.context.AttributeContext;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import pt.psoft.g1.psoftg1.authormanagement.model.Author;
 import pt.psoft.g1.psoftg1.bookmanagement.model.Book;
 import pt.psoft.g1.psoftg1.genremanagement.model.Genre;
@@ -10,12 +13,14 @@ import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
 class BookTest {
+
     private final String validIsbn = "9782826012092";
     private final String validTitle = "Encantos de contar";
-    private final Author validAuthor1 = new Author("João Alberto", "O João Alberto nasceu em Chaves e foi pedreiro a maior parte da sua vida.", null);
-    private final Author validAuthor2 = new Author("Maria José", "A Maria José nasceu em Viseu e só come laranjas às segundas feiras.", null);
-    private final Genre validGenre = new Genre("Fantasia");
+    private final Author mockAuthor = Mockito.mock(Author.class);
+    private final Author mockAuthor2 = Mockito.mock(Author.class);
+    private final Genre mockGenre = Mockito.mock(Genre.class);
     private ArrayList<Author> authors = new ArrayList<>();
 
     @BeforeEach
@@ -25,38 +30,57 @@ class BookTest {
 
     @Test
     void ensureIsbnNotNull(){
-        authors.add(validAuthor1);
-        assertThrows(IllegalArgumentException.class, () -> new Book(null, validTitle, null, validGenre, authors, null));
+
+        // Arrange
+        authors.add(mockAuthor);
+
+        // Act
+        assertThrows(IllegalArgumentException.class, () -> new Book(null, validTitle, null, mockGenre, authors, null));
     }
 
     @Test
     void ensureTitleNotNull(){
-        authors.add(validAuthor1);
-        assertThrows(IllegalArgumentException.class, () -> new Book(validIsbn, null, null, validGenre, authors, null));
+        // Arrange
+        authors.add(mockAuthor);
+
+        // Act
+        assertThrows(IllegalArgumentException.class, () -> new Book(validIsbn, null, null, mockGenre, authors, null));
     }
 
     @Test
     void ensureGenreNotNull(){
-        authors.add(validAuthor1);
+        // Arrange
+        authors.add(mockAuthor);
+
+        // Act
         assertThrows(IllegalArgumentException.class, () -> new Book(validIsbn, validTitle, null,null, authors, null));
     }
 
     @Test
     void ensureAuthorsNotNull(){
-        authors.add(validAuthor1);
-        assertThrows(IllegalArgumentException.class, () -> new Book(validIsbn, validTitle, null, validGenre, null, null));
+
+        // Arrange
+        authors.add(mockAuthor);
+
+        // Act
+        assertThrows(IllegalArgumentException.class, () -> new Book(validIsbn, validTitle, null, mockGenre, null, null));
     }
 
     @Test
     void ensureAuthorsNotEmpty(){
-        assertThrows(IllegalArgumentException.class, () -> new Book(validIsbn, validTitle, null, validGenre, authors, null));
+        // Act
+        assertThrows(IllegalArgumentException.class, () -> new Book(validIsbn, validTitle, null, mockGenre, authors, null));
     }
 
     @Test
     void ensureBookCreatedWithMultipleAuthors() {
-        authors.add(validAuthor1);
-        authors.add(validAuthor2);
-        assertDoesNotThrow(() -> new Book(validIsbn, validTitle, null, validGenre, authors, null));
+
+        // Arrange
+        authors.add(mockAuthor);
+        authors.add(mockAuthor2);
+
+        // Act
+        assertDoesNotThrow(() -> new Book(validIsbn, validTitle, null, mockGenre, authors, null));
     }
 
 }
