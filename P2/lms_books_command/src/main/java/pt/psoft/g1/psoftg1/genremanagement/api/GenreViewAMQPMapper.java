4dee0bfc320bd7497529c00331617488c7f725b2
Pedro Conceição <1211018@isep.ipp.pt>
2025-01-03 22:26:20 +0000

    Updated the GenreViewAMQPMapper to make it consistent with the other models. Changed to an abstract class extending the MapperInterface.

--- P2/lms_books_command/src/main/java/pt/psoft/g1/psoftg1/genremanagement/api/GenreViewAMQPMapper.java
+++ P2/lms_books_command/src/main/java/pt/psoft/g1/psoftg1/genremanagement/api/GenreViewAMQPMapper.java
@@ -1,12 +1,15 @@
 package pt.psoft.g1.psoftg1.genremanagement.api;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import pt.psoft.g1.psoftg1.genremanagement.model.Genre;
+import pt.psoft.g1.psoftg1.shared.api.MapperInterface;
 
 @Mapper(componentModel = "spring")
-public interface GenreViewAMQPMapper {
+public abstract class GenreViewAMQPMapper extends MapperInterface {
 
-    GenreViewAMQP toGenreViewAMQP(Genre genre);
+    @Mapping(target = "version", expression = "java(genre.getVersion().toString())")
+    public abstract GenreViewAMQP toGenreViewAMQP(Genre genre);
 
 
 }
